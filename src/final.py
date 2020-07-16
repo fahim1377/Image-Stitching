c827ea10b3d7bffcfc08c525429b34c74b4f6aa9
fahim <fahim.kammand2@gmail.com>
2020-07-17 02:52:17 +0430

    initial

---
+++ src/final.py
@@ -0,0 +1,61 @@
+import numpy as np
+import cv2
+
+from src.sttitching import *
+
+
+
+def stitch(img1,img2):
+    surf     = cv2.xfeatures2d.SURF_create(400)
+    img1 = img1.astype('uint8')
+    kp1, des1 = surf.detectAndCompute(img1,None)
+    kp2, des2 = surf.detectAndCompute(img2,None)
+
+    # BFMatcher with default params
+    bf      = cv2.BFMatcher()
+    matches = bf.knnMatch(des1,des2,k=2)
+    good_matches = []
+    for m,n in matches:
+        if m.distance < 0.75*n.distance:
+            good_matches.append([m])
+    # good_matches = matches
+    # Select good matched keypoints
+    matched_kpts1   = np.float32([kp1[m[0].queryIdx].pt for m in good_matches])
+    matched_kpts2   = np.float32([kp2[m[0].trainIdx].pt for m in good_matches])
+    # Compute homography
+    H, status    = cv2.findHomography(matched_kpts2, matched_kpts1, cv2.RANSAC,10)
+    # H = cv2.getPerspectiveTransform(matched_kpts2[:4], matched_kpts1[:4])
+    print(H)
+
+    # Warp image
+    # res_im = cv2.warpPerspective(img2,M=H,dsize=(img1.shape[1]+img2.shape[1], img1.shape[0]))
+    res_im = stitchIm(matched_kpts1,matched_kpts2,img1,img2,H)
+
+    return res_im
+
+def main():
+    mountains = ['Mountain_2','Mountain_1','Mountain_3','Mountain_5','Mountain_4','Mountain_6','Mountain_7']
+    # mountains = ['Mountain_5','Mountain_2','Mountain_1','Mountain_4','Mountain_3','Mountain_7','Mountain_6']
+    offices = ['office-00','office-01','office-02','office-03']
+    towers = ['Tower_2','Tower_1']
+    yards = ['yard-04','yard-05','yard-03','yard-02','yard-01','yard-06','yard-07','yard-08']
+    imlist = mountains
+    ref_im = cv2.imread('../images/Input/{}.jpg'.format(imlist[0]))
+    for i,imName in enumerate(imlist[1:]):
+        scene_im = cv2.imread('../images/Input/{}.jpg'.format(imName))
+        ref_im = stitch(ref_im,scene_im)
+        print("here")
+        cv2.imwrite('../images/results/result{}.jpg'.format(imName), ref_im)
+
+
+    # ref_im = median_filter(ref_im,(3,3))
+    # print("median1")
+    # ref_im = median_filter(ref_im,(3,3))
+    # print("median2")
+    # ref_im = median_filter(ref_im,(3,3))
+    # print("median3")
+
+    cv2.imwrite('../images/results/result{}.jpg'.format(imName), ref_im)
+    # cv2.imwrite('../images/results/fft-2-{}.jpg'.format(imName), showRES)
+
+main()
---
+++ src/sttitching.py
@@ -0,0 +1,150 @@
+import numpy as np
+import cv2
+import copy
+import cmath
+
+
+def interpolation(img):
+    cop_im = copy.deepcopy(img)
+    for i in range(0,img.shape[0]):
+        for j in range(0,img.shape[1]):
+            for k in range(0,img.shape[2]):
+                if (img[i,j,k] == 1):
+                    find = False
+                    radius = 1
+                    while(not find):
+                        try:
+                            if (img[i+radius,j,k] != 1):
+                                cop_im[i,j] = img[i+radius,j]
+                                find = True
+                        except:
+                            pass
+                        try:
+                            if img[i-radius,j,k] != 1:
+                                cop_im[i,j] = img[i-radius,j]
+                                find = True
+                        except:
+                            pass
+                        try:
+                            if img[i,j+radius,k] != 1:
+                                cop_im[i,j] = img[i,j+radius]
+                                find = True
+                        except:
+                            pass
+                        try:
+                            if img[i,j-radius,k] != 1:
+                                cop_im[i,j] = img[i,j-radius]
+                                find = True
+                        except:
+                            pass
+                        if radius == 2:
+                            find = True
+                        radius += 1
+
+    return cop_im
+
+def median_filter(img,sizeWindow):
+    res = copy.deepcopy(img)
+    paddingW2cntri = int(sizeWindow[0]/2)
+    paddingW2cntrj = int(sizeWindow[1]/2)
+    for i in range(paddingW2cntri,img.shape[0]-paddingW2cntri):
+        for j in range(paddingW2cntrj,img.shape[1]-paddingW2cntrj):
+            for k in range(3):
+                if(img[i,j,k]==1 ):
+                    window = np.sort(np.asarray(img[i-paddingW2cntri:i+paddingW2cntri+1,j-paddingW2cntrj:j+paddingW2cntrj+1,k]).flatten())
+                    res[i,j,k] = window[int(sizeWindow[0]*sizeWindow[1]/2)]
+    return res
+
+
+def max_filter(img,sizeWindow):
+    res = copy.deepcopy(img)
+    paddingW2cntri = int(sizeWindow[0]/2)
+    paddingW2cntrj = int(sizeWindow[1]/2)
+    for i in range(paddingW2cntri,img.shape[0]-paddingW2cntri):
+        for j in range(paddingW2cntrj,img.shape[1]-paddingW2cntrj):
+            for k in range(3):
+                if(img[i,j,k]==1 ):
+                    window = np.sort(np.asarray(img[i-paddingW2cntri:i+paddingW2cntri+1,j-paddingW2cntrj:j+paddingW2cntrj+1,k]).flatten())
+                    res[i,j,k] = window[int(sizeWindow[0]*sizeWindow[1])-1]
+    return res
+
+def toPolar(F):
+    FA = abs(F)
+    FP = np.zeros(F.shape,dtype=float)
+    for i in range(0,F.shape[0]):
+        for j in range(0,F.shape[1]):
+            FP[i,j] = cmath.phase(F[i,j])
+    return FA,FP
+
+
+def normalizeimg(img,min,max):
+    img = (img-min)/(max-min)*255
+    return img
+
+
+def stitchIm(kps1,kps2,rgb_im1,rgb_im2,h):
+    size_im1 = rgb_im1.shape
+
+    size_im2 = rgb_im2.shape
+    S = 2
+    res_im = np.ones((int(size_im1[1]+S*size_im2[1]),int(size_im1[0]+S*size_im2[0]),3),dtype=np.uint8)
+    print(res_im.shape)
+    res_im[int(S/2)*rgb_im2.shape[0]:int(S/2)*rgb_im2.shape[0] + rgb_im1.shape[0]
+    , int(S/2)*rgb_im2.shape[1]:int(S/2)*rgb_im2.shape[1] + rgb_im1.shape[1]] = rgb_im1
+
+    for i in range(0,size_im2[0]):
+        for j in range(0,size_im2[1]):
+            for k in range(3):
+                xnew = int( ((h[0,0]*j)+(h[0,1]*i)+h[0,2]) /
+                            ((h[2,0]*j)+(h[2,1]*i)+h[2,2]))
+                ynew = int( ((h[1,0]*j)+(h[1,1]*i)+h[1,2])
+                            /((h[2,0]*j)+(h[2,1]*i)+h[2,2]))
+                xnew += int(S/2)*size_im2[1]
+                ynew += int(S/2)*size_im2[0]
+                res_im[ynew,xnew,k] = rgb_im2[i,j,k]
+        # print("row")
+    xmax = np.argmax(res_im, axis=1)
+    minRow = 0
+    for i in range(xmax.shape[0]):
+        if (xmax[i,0]!=0 or xmax[i,1]!=0 or xmax[i,2]!=0 ):
+            minRow = i
+            print(minRow)
+            break;
+    maxRow = 0
+    for i in range(xmax.shape[0]-1,0,-1):
+        if (xmax[i,0]!=0 or xmax[i,1]!=0 or xmax[i,2]!=0 ):
+            maxRow = i
+            print(maxRow)
+            break;
+
+    ymax = np.argmax(res_im, axis=0)
+    minCol = 0
+    for i in range(ymax.shape[0]):
+        if (ymax[i,0]!=0 or ymax[i,1]!=0 or ymax[i,2]!=0 ):
+            minCol = i
+            print(minCol)
+            break;
+    maxCol = 0
+    for i in range(ymax.shape[0]-1,0,-1):
+        if (ymax[i,0]!=0 or ymax[i,1]!=0 or ymax[i,2]!=0 ):
+            maxCol = i
+            print(maxCol)
+            break;
+    res_im = res_im[minRow:maxRow,minCol:maxCol,:]
+
+    # fp = res_im[:,:,2]
+    # Fuv = np.fft.fft2(fp)
+    # FuvA,FuvP = toPolar(Fuv)
+    # FuvA = np.fft.fftshift(FuvA)
+    # FuvA += 1
+    # showRES = np.log10(FuvA)
+    # min = np.min(showRES)
+    # max = np.max(showRES)
+    # showRES = normalizeimg(showRES,min,max)
+
+    res_im = max_filter(res_im,(3,3))
+    # print("median1")
+    cv2.imwrite("res.jpg",res_im)
+    # cv2.imwrite("freqRes.jpg",showRES)
+    return res_im
+
